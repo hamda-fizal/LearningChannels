8fdc4c8e22b56c519b131e72952fbf787763b5ad
hamda-fizal <hamda.fzl@gmail.com>
2023-04-03 23:02:57 +0530

    add assembly ai speech-to-text functionality

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -26,12 +26,15 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        if (text_data_json):
+            print(text_data)
+            message = text_data_json["message"]
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
+            # Send message to room group
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name, {
+                    "type": "chat_message", "message": message}
+            )
 
     # Receive message from room group
     def chat_message(self, event):
--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -1,4 +1,5 @@
 # chat/views.py
+import requests
 from django.shortcuts import render
 
 
@@ -7,4 +8,16 @@ def index(request):
 
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    headers = {"authorization": ""}  # provide Assembly AI API key here.
+    data = {"expires_in": 3600}
+    token = ""
+    try:
+        response = requests.post(
+            'https://api.assemblyai.com/v2/realtime/token', json=data, headers=headers)
+        response_json = response.json()
+        token = response_json['token']
+
+    except Exception as e:
+        print(e)
+
+    return render(request, "chat/room.html", {"room_name": room_name, "token": token})
